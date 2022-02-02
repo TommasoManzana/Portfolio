c9bdf970b5624d9522875f9ee783e3a83ce8f6c4
TommasoManzana <manzana.tommaso@gmail.com>
2022-02-02 15:41:00 +0100

    Added typing subheader

--- src/components/landing-bio.js
+++ src/components/landing-bio.js
@@ -2,9 +2,9 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 import styled from "@emotion/styled"
-import { SocialIcon } from 'react-social-icons';
+import { SocialIcon } from "react-social-icons"
 
-import Typist from 'react-typist';
+import Typist from "react-typist"
 
 const Container = styled.div`
   text-align: center;
@@ -14,14 +14,15 @@ const OuterContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  flex-direction: row;
+  flex-direction: column;
   height: 78vh;
 `
 
 const Description = styled.p`
   padding: 0;
-  margin-bottom: 4rem;
+  margin-bottom: 3rem;
   font-size: 1.4rem;
+  min-height: 25px;
 `
 
 const NameHeader = styled.h1`
@@ -33,7 +34,7 @@ const SocialLinks = styled.div`
   width: 100%;
   display: inline-block;
 `
-const Social = { marginRight: '10px', display: 'inline-block' };
+const Social = { marginRight: "10px" }
 
 const LandingBio = () => (
   <StaticQuery
@@ -47,15 +48,34 @@ const LandingBio = () => (
         }
       }
     `}
-    render={data => (
+    render={(data) => (
       <OuterContainer>
         <Container>
-          <NameHeader><Typist cursor={{ hideWhenDone: true }}>Tommaso Manzana</Typist></NameHeader>
-          <Description>{data.site.siteMetadata.subtitle}</Description>
-          
+          <NameHeader>
+            <Typist cursor={{ hideWhenDone: true }}>Tommaso Manzana</Typist>
+          </NameHeader>
+          <Description>
+            <Typist cursor={{ hideWhenDone: true }}>
+              <Typist.Delay ms={1500} />
+              Student
+              <Typist.Backspace count={7} delay={1000} />
+              {data.site.siteMetadata.subtitle}
+              <Typist.Backspace count={18} delay={1000} />
+              ML enthusiast
+              <Typist.Backspace count={13} delay={1000} />
+            </Typist>
+          </Description>
+        </Container>
+        <Container>
           <SocialLinks>
-            <SocialIcon style={Social} url="https://twitter.com/TommasoManzana" />
-            <SocialIcon style={Social} url="https://www.linkedin.com/in/tommaso-manzana/" />
+            <SocialIcon
+              style={Social}
+              url="https://twitter.com/TommasoManzana"
+            />
+            <SocialIcon
+              style={Social}
+              url="https://www.linkedin.com/in/tommaso-manzana/"
+            />
             <SocialIcon url="https://www.instagram.com/tommaso.manzana/" />
           </SocialLinks>
         </Container>
